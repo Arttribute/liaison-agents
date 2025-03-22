53cbf3af04f2207428bf0e023107a7156b8dd3d4
bashybaranaba <bashybaranaba@gmail.com>
2025-03-22 00:29:52 +0000

    feat: add dashboard sidebar

--- apps/liaison-app/app/studio/page.tsx
+++ apps/liaison-app/app/studio/page.tsx
@@ -1,12 +1,15 @@
 import AppBar from "@/components/layout/app-bar";
 import { LiasonsTable } from "@/components/studio/liaisons-table";
+import { SidebarNav } from "@/components/studio/sidebar-nav";
 
 export default function Home() {
   return (
     <div>
       <AppBar />
-      <div className="mt-12">
-        <div className="m-4">
+      <div className="flex mt-12 overflow-hidden">
+        <SidebarNav />
+
+        <div className="m-4 w-full">
           <LiasonsTable />
         </div>
       </div>
---
+++ apps/liaison-app/components/studio/sidebar-nav.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  PanelLeft,
+  BookOpen,
+  PlusCircle,
+  Sparkles,
+  ListIcon,
+} from "lucide-react";
+
+export function SidebarNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        "h-screen bg-background border-r border-border flex flex-col transition-all duration-300",
+        isOpen ? "w-[220px] min-w-[220px]" : "w-[60px] min-w-[60px]"
+      )}
+    >
+      <div className="px-3 pt-4 flex items-center justify-between">
+        {isOpen ? (
+          <button
+            onClick={() => setIsOpen(false)}
+            className="mx-2 text-muted-foreground hover:text-foreground"
+          >
+            <PanelLeft className="h-5 w-5" />
+          </button>
+        ) : (
+          <div className="px-2">
+            <button
+              onClick={() => setIsOpen(true)}
+              className=" items-center justify-center text-muted-foreground hover:text-foreground"
+            >
+              <PanelLeft className="h-5 w-5" />
+            </button>
+          </div>
+        )}
+      </div>
+
+      <div className="px-3 py-2">
+        <Link href={`/worlds/create`}>
+          <Button className="w-full flex items-center justify-center gap-2  rounded-md py-2 font-medium text-sm">
+            {isOpen ? (
+              <>
+                <PlusCircle className="h-4 w-4" />
+                New liaison agent
+              </>
+            ) : (
+              <PlusCircle className="h-5 w-5" />
+            )}
+          </Button>
+        </Link>
+      </div>
+
+      <nav className="mt-2 px-3">
+        <ul className="space-y-1">
+          <li>
+            <Link
+              href="/studio"
+              className="flex items-center gap-3 text-muted-foreground hover:text-foreground py-2 px-2 rounded-md hover:bg-accent transition-colors"
+            >
+              <ListIcon className="h-5 w-5" />
+              {isOpen && <span className="text-sm">All liaisons</span>}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/studio"
+              target="_blank"
+              className="flex items-center gap-3 text-muted-foreground hover:text-foreground py-2 px-2 rounded-md hover:bg-accent transition-colors"
+            >
+              <BookOpen className="h-5 w-5" />
+              {isOpen && <span className="text-sm">How to</span>}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
